c83f39d2476a181d921583382810614319877478
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2020-06-18 12:05:37 +0200

    Added some static content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     ]
   },
   {
-    path: '/admin', loadChildren: './admin/admin.module#AdminModule'
+    path: 'admin', loadChildren: './admin/admin.module#AdminModule'
   }
 ];
 
