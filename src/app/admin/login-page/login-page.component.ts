9ee6783ed3dfbc9444bd7d6fb2a8192c6fb7b723
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2020-06-22 12:04:30 +0200

    Working process
    continue previous

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {User} from '../shared/interfaces';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -11,7 +13,10 @@ export class LoginPageComponent implements OnInit {
 
   form: FormGroup
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -31,6 +36,9 @@ export class LoginPageComponent implements OnInit {
 
     }
 
-
+    this.auth.login(user).subscribe( () => {
+      this.form.reset()
+      this.router.navigate(['/admin', 'dashboard'])
+    })
   }
 }
